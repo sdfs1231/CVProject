d37b2d4d89a665febd6d48ace793d0af9b8de023
sdfs1231 <50002531+sdfs1231@users.noreply.github.com>
2019-11-11 20:36:03 +0800

    k-means
    class k-means

---
+++ test.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# a={'x': [12, 20, 28, 18, 10, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72, 23]}
+# print(max(a['x']))
+
+class kmeans():
+    def __init__(self,dataframe,k,centerid={},colormap={}):
+        self.dataframe=dataframe
+        self.k=k
+        self.centerid={i:[np.random.randint(min(self.dataframe['x']),max(self.dataframe['x'])),np.random.randint(min(self.dataframe['y']),max(self.dataframe['y']))] for i in range(self.k)}
+        self.colormap={0:'r',1:'g',2:'b'}
+
+    def paint_color(self):
+        for i in self.centerid.keys():
+            self.dataframe['distance_from_{}'.format(i)]=np.sqrt((self.dataframe['x']-self.centerid[i][0])**2+(self.dataframe['y']-self.centerid[i][1])**2)
+        distance_from_centroid_id = ['distance_from_{}'.format(i) for i in self.centerid.keys()]
+        self.dataframe['closest'] = self.dataframe.loc[:, distance_from_centroid_id].idxmin(axis=1)
+        self.dataframe['closest'] = self.dataframe['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+        self.dataframe['color']=self.dataframe['closest'].map(lambda x:self.colormap[x])
+        return self.dataframe
+
+    def update(self):
+        for i in self.centerid.keys():
+            self.centerid[i][0]=np.mean(self.dataframe[self.dataframe['closest']==i]['x'])
+            self.centerid[i][1] = np.mean(self.dataframe[self.dataframe['closest'] == i]['y'])
+        return self.centerid
+
+    def showmap(self):
+        plt.scatter(self.dataframe['x'], self.dataframe['y'], color=self.dataframe['color'], alpha=0.5, edgecolor='k')
+        for i in self.centerid.keys():
+            plt.scatter(*self.centerid[i], color=self.colormap[i], linewidths=6)
+        plt.xlim(0, 80)
+        plt.ylim(0, 80)
+        plt.show()
+
+    def kmeans_fun(self):
+        self.paint_color()
+        while True:
+            closest_centroids = self.dataframe['closest'].copy(deep=True)
+            self.centroids = self.update()
+
+            plt.scatter(self.dataframe['x'], self.dataframe['y'], color=self.dataframe['color'], alpha=0.5, edgecolor='k')
+            for i in self.centerid.keys():
+                plt.scatter(*self.centerid[i], color=self.colormap[i], linewidths=6)
+            plt.xlim(0, 80)
+            plt.ylim(0, 80)
+            plt.show()
+
+            self.dataframe = self.paint_color()
+
+            if closest_centroids.equals(self.dataframe['closest']):
+                break
+
+
+df = pd.DataFrame({
+        'x': [12, 20, 28, 18, 10, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72, 23],
+        'y': [39, 36, 30, 52, 54, 20, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24, 77]
+    })
+
+test=kmeans(df,3)
+test.kmeans_fun()
